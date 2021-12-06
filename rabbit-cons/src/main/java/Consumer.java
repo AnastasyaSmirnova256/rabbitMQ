1835546c1eacdb42032845d886192b9340befa87
AnastasyaSmirnova256 <75490520+AnastasyaSmirnova256@users.noreply.github.com>
2021-12-07 00:55:52 +0300

    Update Consumer.java

--- rabbit-cons/src/main/java/Consumer.java
+++ rabbit-cons/src/main/java/Consumer.java
@@ -9,14 +9,18 @@ public class Consumer {
     private static final String QUEUE_NAME = "products_queue";
 
     public static void main (String[] args) throws IOException, TimeoutException {
+        //объявление соединения к серверу RabbitMQ и открытие канала сообщений
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
+        
+        //объявление устойчивой очереди с отключенными свойствами эксклюзивности и автоудаления
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
+        
+        //его величество consumer
         DefaultConsumer consumer = new DefaultConsumer(channel) {
+            //метод обработки сообщения
             @Override
             public void handleDelivery(String consumerTag,
                                        Envelope envelope, AMQP.BasicProperties properties,
@@ -25,6 +29,7 @@ public class Consumer {
                 System.out.println(" [x] Received '" + message + "'");
             }
         };
+        // подключение consumer к очереди
         channel.basicConsume(QUEUE_NAME, true, consumer);
     }
 }
